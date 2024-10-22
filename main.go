1419ca18a3cd9248b14a72bf5702e32b8a663f63
nakitabrown <97200962+nakitabrown@users.noreply.github.com>
2024-10-22 19:13:07 -0400

    Add files via upload
    Updated main file.

--- main.go
+++ main.go
@@ -1,23 +1,100 @@
 package main
 
-import "fmt"
+import (
+	"fmt" //print
+	"strings"
+)
 
+// PACKAGE LEVEL VARIABLES AVAILABLE TO ALL FUNCS
+// conferenceName := "Go Conference" | doesn't work with constants nor at package level variables
+// Go can determine the data type without declaring it
+const conferenceTickets int = 50
+
+var conferenceName string = "Go Conference"
+var remainingTickets uint = 50
+var bookings = []string{}
+
+// MAIN FUNCTION
 func main() {
-	var conferenceName string = "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	var bookings = [50]string{}
 
+	//GREETING FUNCTION
+	greetUsers()
+
+	for {
+		//USER INPUT FUNCTION
+		firstName, lastName, email, userTickets := getUserInput()
+
+		//INPUT VALIDATION FUNCTION
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+			//BOOK TICKETS FUNCTION
+			bookTicket(userTickets, firstName, lastName, email)
+
+			//FIRST NAMES FUNCTION
+			firstNames := getFirstNames()
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				//end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break //break for loop
+			}
+
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name you entered is too short.")
+			}
+
+			if !isValidEmail {
+				fmt.Println("Email address you entered does not contain @ sign.")
+			}
+
+			if !isValidTicketNumber {
+				fmt.Println("Number of tickets you entered is invalid.")
+			}
+
+		}
+
+	}
+
+}
+
+// FUNCTIONS
+func greetUsers() {
+	//variable can be the same name as parameter or different
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
+}
 
+// []STRING IS OUTPUT
+func getFirstNames() []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames //PRINT FIRST NAMES - OUTPUT
+}
+
+// INPUT VALIDATION
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+	return isValidName, isValidEmail, isValidTicketNumber //GO CAN RETURN MULTIPLE VALUES
+}
+
+// USER INPUT
+// CREATE VARIABLES LOCAL AS POSSIBLE
+func getUserInput() (string, string, string, uint) {
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
 
-	//ask user information
+	//USER INPUT
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
 
@@ -30,22 +107,17 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
-	bookings[0] = firstName + " " + lastName
+	return firstName, lastName, email, userTickets
+}
 
-	//Print Array Information
-	fmt.Printf("The whole array: %v\n", bookings)
-	fmt.Printf("Print the first value: %v\n", bookings[0])
-	fmt.Printf("Array Type: %v\n", bookings)
-	fmt.Printf("Array Length: %v\n", bookings)
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	//SLICE
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("User %v booked %v tickets.\n", firstName, userTickets)
-
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for the %v.\n", remainingTickets, conferenceName)
-
-	//QUICK NOTES
-	//conferenceName := "Go Conference" | doesn't work with constants
-	//Go can determine the data type without declaring it
+	return
 
 }
